82e0327c9eb0b33caf23a72b187d356611f80734
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-10-02 09:16:14 +0300

    created hero table

--- app/models.py
+++ app/models.py
@@ -3,8 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Hero(db.Model):
-    __tablename__ = 'hero'
+    __tablename__ = 'heroes'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    super_name = db.Column(db.String(10), nullable=False)
+    created_at = db.Column(db.DateTime,server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    powers = db.relationship('Heropower',backref='heroes')
 
-# add any models you may need. 
