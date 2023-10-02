c74d5f228d8c68692fa0f6845a08ba03fa1719ab
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-10-02 10:48:23 +0300

    created hero_powers table

--- app/models.py
+++ app/models.py
@@ -24,3 +24,16 @@ class Power(db.Model):
   
     heroes = db.relationship('Heropower',backref='powers')
   
+class Hero_power(db.Model):
+    __tablename__ = 'hero_powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String(100), nullable=False)
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
+    created_at = db.Column(db.DateTime,server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+        
+        
+
+
