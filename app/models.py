bf84c0a9bb10b7ca8cf9009a6735bb560c54a7c1
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-10-04 14:56:49 +0300

    added validations

--- app/models.py
+++ app/models.py
@@ -1,6 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
+from sqlalchemy import MetaData, ForeignKey
 
-db = SQLAlchemy()
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
+
 
 class Hero(db.Model):
     __tablename__ = 'heroes'
@@ -11,7 +18,11 @@ class Hero(db.Model):
     created_at = db.Column(db.DateTime,server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
     
-    powers = db.relationship('Heropower',backref='heroes')
+    powers = db.relationship('Hero_powers',back_populates='hero')
+    
+    def __repr__(self):
+        return f'(id={self.id}, name={self.name} super_name={self.super_name})'
+    
 
 class Power(db.Model):
     __tablename__ = 'powers'
@@ -22,9 +33,22 @@ class Power(db.Model):
     created_at = db.Column(db.DateTime,server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
   
-    heroes = db.relationship('Heropower',backref='powers')
-  
-class Hero_power(db.Model):
+    heroes = db.relationship('Hero_powers',back_populates='power')
+    
+    def __repr__(self):
+        return f'(id={self.id}, name={self.name} description={self.description})'
+    
+    @validates("description")
+    def validate_description(self, key, description):
+        if  len(description) < 20:
+            raise ValueError("description must be present and at least 20 characters long")
+        else:
+            return description
+            
+
+        
+    
+class Hero_powers(db.Model):
     __tablename__ = 'hero_powers'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -33,6 +57,19 @@ class Hero_power(db.Model):
     power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
     created_at = db.Column(db.DateTime,server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    hero = db.relationship('Hero', back_populates='powers')
+    power = db.relationship('Power', back_populates='heroes')
+    
+    def _repr_(self):
+        return f'(id={self.id}, heroID={self.hero_id} strength={self.strength}) powerID={self.power_id}'
+
+    @validates('strength')
+    def checks_strength(self, key, strength):
+        if strength not in ['Strong', 'Weak', 'Average']:
+            raise ValueError("Strength must be a value either 'Strong', 'Weak' or 'Average'")
+        else:
+            return strength
         
         
 
