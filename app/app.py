625808ddff14e38c5dcfeaaa2340ca84243f2728
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-10-04 15:08:57 +0300

    created home class

--- app/app.py
+++ app/app.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3
-
-from flask import Flask, make_response
+from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from werkzeug.exceptions import NotFound
 
-from models import db, Hero
+from models import db,Hero,Power,Hero_powers
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
-@app.route('/')
-def home():
-    return ''
+api = Api(app)
+# create a response for landing page
+
+class Home(Resource):
+    def get(self):
+        response_message = {
+            "message": "WELCOME TO THE PIZZA RESTAURANT API."
+        }
+        return make_response(response_message, 200)
 
 
-if __name__ == '__main__':
-    app.run(port=5555)
+api.add_resource(Home, '/')
+
